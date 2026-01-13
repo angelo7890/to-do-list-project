8aa69173af81f25915f1b7f038afd0d8b684995d
angelo7890 <ar293978@gmail.com>
2026-01-13 00:58:15 -0300

    criacao das controller de user e task

---
+++ to-do-list/src/main/java/com/example/demo/controller/TaskController.java
@@ -0,0 +1,28 @@
+﻿package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @PostMapping
+    public void createTask() {
+    }
+
+    @GetMapping
+    public void getTask() {
+    }
+
+    @GetMapping
+    public void getAllTasks() {
+    }
+
+    @PatchMapping
+    public void updateTask() {
+    }
+
+    @DeleteMapping
+    public void deleteTask() {
+    }
+}
---
+++ to-do-list/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,27 @@
+﻿package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/user")
+public class UserController {
+
+    @PostMapping
+    public void createUser() {
+    }
+
+    @GetMapping
+    public void getUser() {
+    }
+
+    @GetMapping
+    public void getAllUsers() {
+    }
+
+    @PatchMapping
+    public void updateUser() {
+    }
+
+    @DeleteMapping
+    public void deleteUser() {
+    }
+}
