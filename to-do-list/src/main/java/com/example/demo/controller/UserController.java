6181280a258cc0c3a2b43ac3c2901311a7a5ab92
angelo7890 <ar293978@gmail.com>
2026-01-29 02:10:12 -0300

    foi criado as funcoes da controller de user

--- to-do-list/src/main/java/com/example/demo/controller/UserController.java
+++ to-do-list/src/main/java/com/example/demo/controller/UserController.java
@@ -1,27 +1,49 @@
 ﻿package com.example.demo.controller;
+import com.example.demo.dto.request.RequestCreateUserDto;
+import com.example.demo.dto.request.RequestUpdateUserDto;
+import com.example.demo.dto.response.ResponsePaginationUserDto;
+import com.example.demo.dto.response.ResponseUserDto;
+import com.example.demo.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/user")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
-    public void createUser() {
+    public ResponseEntity<?> createUser(@Valid @RequestBody RequestCreateUserDto dto) {
+        userService.createUser(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseUserDto> getUser(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
     }
 
     @GetMapping
-    public void getUser() {
+    public ResponseEntity<ResponsePaginationUserDto> getAllUsers(@RequestParam int page, @RequestParam int size) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAllUsers(page, size));
     }
 
-    @GetMapping
-    public void getAllUsers() {
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable Long id, @Valid @RequestBody RequestUpdateUserDto dto) {
+        userService.updateUserById(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
-    @PatchMapping
-    public void updateUser() {
-    }
-
-    @DeleteMapping
-    public void deleteUser() {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
